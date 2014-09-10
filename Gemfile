80be810b7a3ce0a02e8a0559f6763b98882d6513
arcoiris <deborlora@gmail.com>
2014-09-09 21:57:34 -0400

    Add expenditure controller, create, and update functionality

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.1.0'
-gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -14,7 +12,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bcrypt', '~> 3.1.7'
 
-# Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-gem 'spring',        group: :development
-
-
+group :development do 
+	gem 'pry'
+	gem 'spring' 
+	gem 'sqlite3'
+end
---
+++ app/controllers/expenditures_controller.rb
@@ -0,0 +1,49 @@
+class ExpendituresController < ApplicationController
+	before_action :set_expenditure, only: [:edit, :update, :show, :destroy]
+
+	def index
+	end
+
+	def new
+		@expenditure = Expenditure.new
+	end
+
+	def create
+		@expenditure = Expenditure.new(safe_params)
+		@expenditure.user = User.first #change this when sessions are added
+		if @expenditure.save
+			flash[:notice] = "Your expenditure has been added."
+			redirect_to :back
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @expenditure.update(safe_params)
+			flash[:notice] = "Your expenditure has been updated."
+			redirect_to :back
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	private
+
+	def safe_params
+		params.require(:expenditure).permit(:name, :amount, :description)
+	end
+
+	def set_expenditure
+		@expenditure = Expenditure.find(params[:id])
+	end
+end
