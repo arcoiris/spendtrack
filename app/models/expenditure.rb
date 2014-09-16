ed5d9deafe3ebdeb9148e99cb61e9d6cdf48e8e1
arcoiris <deborlora@gmail.com>
2014-09-15 20:51:33 -0400

    Add nav bar, expenditure validations, style spending form

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -4,5 +4,7 @@ class Expenditure < ActiveRecord::Base
 	has_many :expenditure_categories
 	has_many :categories, through: :expenditure_categories
 
-	validates :amount, presence: true
+	validates :amount, presence: true, numericality: true, numericality: { greater_than: 0 }
+	validates :name, length: { maximum: 50 }
+	validates :description, length: { maximum: 500 }
 end
